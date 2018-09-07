60d457bb13662a377535fd128044e084569fe1d1
Napoleon Wulkan <napoleon@wulkan.xyz>
2018-09-07 14:48:00 +0200

    added endpoint for gtting all triggers

--- triggers.go
+++ triggers.go
@@ -29,6 +29,24 @@ func (s *TriggersService) GetTrigger(triggerID int64) (*dto.TriggerDTO, error) {
 	return responseDTO, nil
 }
 
+func (s *TriggersService) GetTriggers() (*[]dto.TriggerDTO, error) {
+	response := s.client.carmaRequest("triggers", http.MethodGet, nil)
+	if response.err != nil {
+		return nil, response.err
+	}
+
+	fmt.Println(string(response.data))
+
+	responseDTO := &[]dto.TriggerDTO{}
+
+	err := json.Unmarshal(response.data, responseDTO)
+	if err != nil {
+		return nil, err
+	}
+
+	return responseDTO, nil
+}
+
 func (s *TriggersService) PostTriggersMessages(triggerID int64, triggerDTO dto.SendTriggerDTO) (*dto.MessageDTO, error) {
 	response := s.client.carmaRequest(fmt.Sprintf("triggers/%d/messages", triggerID), http.MethodPost, triggerDTO)
 	if response.err != nil {
